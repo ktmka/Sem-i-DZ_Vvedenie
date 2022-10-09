d5215d3a8443b8d6db98f06306481d8b35bf0b28
ktmka <tomatimoto@gmail.com>
2022-10-09 19:50:47 +0500

    add SEMINAR_2

---
+++ SEMINAR_2/Domashka/Domashka.cs
@@ -0,0 +1,64 @@
+﻿// Задача 10. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+// Console.WriteLine("add number");
+// int number = int.Parse(Console.ReadLine()!);
+// if (number > 99 && number <= 999)
+// {
+//     int num1 = number%100;
+//     int num2 = num1/10;
+//     Console.WriteLine(num2);
+// }
+// else{
+//     Console.WriteLine("Ошибка: это число не трехзначное!");
+// }
+
+
+// // Задача 13. Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет
+// Вариант 1
+// Console.WriteLine("add number");
+// int number = int.Parse(Console.ReadLine()!);
+// int num3 = 0;
+// if (number >= 0 && number <= 100000)
+// {
+//     if (number >= 0 && number < 100)        Console.WriteLine("число не имеет третьей цифры");
+//     if (number >= 100 && number < 1000)
+//     {
+//         num3 = number%10; 
+//         Console.WriteLine(num3);
+//     }                               
+//     if (number >= 1000 && number < 10000)
+//     {
+//         num3 = number%100; 
+//         num3 = num3/10; 
+//         Console.WriteLine(num3);
+//     }                                        
+//     if (number >= 10000 && number < 100000)
+//     {
+//         num3 = number%1000; 
+//         num3 = num3/100; 
+//         Console.WriteLine(num3);
+//     }                                      
+//     if (number == 100000){
+//         num3 = number%10000; 
+//         num3 = num3/1000;
+//         Console.WriteLine(num3);
+//     }                                         
+// }
+// else{
+//     Console.WriteLine("число выходит за заданные границы");
+// }
+
+
+// Задача 15. Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным 
+Console.WriteLine("задайте номер дня недели");
+int day = int.Parse(Console.ReadLine()!);
+if (day >= 1 && day <= 7)
+{
+    if (day == 7 || day == 6)    Console.WriteLine("Да, это выходной!");
+    else                         Console.WriteLine("Нет, это не выходной ((");
+}
+else{
+    Console.WriteLine("Ошибка: неверный номер");
+}
+
---
+++ SEMINAR_2/Задачи семинара/Program.cs
@@ -0,0 +1,92 @@
+﻿// Вывести случайное число из отрезка [10, 99] и показать наибольшую цифру числа
+// int number = new Random().Next(10, 100);
+// int num1 = number/10;
+// int num2 = number%10;
+// Console.WriteLine(number);
+// if (num1 > num2)
+// {
+//     Console.WriteLine(num1);
+// }
+// else
+// {
+//     Console.WriteLine(num2);
+// }
+
+// Вариант 2 -----------------------------------
+// int number = new Random().Next(10, 100);
+// int num1 = number/10;
+// int num2 = number%10;
+// Console.WriteLine(number);
+// int max = num1;
+// if (max < num2)
+// {
+//     max = num2;
+// }
+// Console.WriteLine(max);
+
+// Задача1 ----------------------------------
+// Вывести трехзначное случайное число и удалить вторую цифру этого числа
+// int number = new Random().Next(100, 999);
+// int num1 = number/100;
+// int num2 = number%10;
+// Console.WriteLine(number);
+// Console.WriteLine($"{num1}{num2}");
+// Console.Write(num1);
+// Console.Write(num2);
+
+// Задача2 ----------------------------------
+// Принимать на вход два числа и вывести, яляется ли второе число кратным первому. Если число не кратно, вывести остаток
+// int number1 = new Random().Next(1, 1000);
+// int number2 = new Random().Next(1, 1000); // Для эксперимента рандом
+// Console.WriteLine(number1);
+// Console.WriteLine(number2);
+// int number3 = (number2%number1);
+// if (number3 == 0)
+// {
+//     Console.WriteLine($"{number2} кратно {number1}");
+// }
+// else{
+//     Console.WriteLine($"{number2} не кратно {number1}. Остаток = {number3}"); 
+// }
+
+// Вариант как в задаче  
+// Console.WriteLine("add first number");
+// int number1 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("add second number");
+// int number2 = int.Parse(Console.ReadLine()!); // как в задаче
+// Console.WriteLine(number1);
+// Console.WriteLine(number2);
+// int number3 = (number2%number1);
+// if (number3 == 0)
+// {
+//     Console.WriteLine($"{number2} кратно {number1}");
+// }
+// else{
+//     Console.WriteLine($"{number2} не кратно {number1}. Остаток = {number3}"); 
+// }
+
+// Задача 3. Введенное число кратно ли 7 и 23 (и) ---------------------
+// Console.WriteLine("add number");
+// int number = int.Parse(Console.ReadLine()!);
+// if (number%7 == 0 && number%23 == 0)
+// {
+//     Console.WriteLine($"{number} кратно 7 и 23");
+// }
+// else{
+//     Console.WriteLine($"{number} не кратно 7 и 23"); 
+// }
+
+// Задача 4. Введенные два числа, является лм одно квадратом другого?  (или)---------------
+Console.WriteLine("add first number");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("add second number");
+int number2 = int.Parse(Console.ReadLine()!); 
+Console.WriteLine(number1);
+Console.WriteLine(number2);
+if (number1 == Math.Pow(number2, 2)  || number2 == Math.Pow(number1, 2))
+{
+    Console.WriteLine("одно число квадрат другого");
+}
+else{
+    Console.WriteLine("отрицательный ответ"); 
+}
