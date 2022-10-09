7086086a05a26a0830e0f9d2471ef1cdb1103971
ktmka <tomatimoto@gmail.com>
2022-10-09 19:47:21 +0500

    add Seminar1

---
+++ Seminar1/Domachka.cs
@@ -0,0 +1,56 @@
+﻿// Задача 2. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// Console.WriteLine("Введи первое число");
+// int num1 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введи второе число");
+// int num2 = int.Parse(Console.ReadLine()!);
+// if (num1 > num2)
+// {
+//     Console.WriteLine($"max = {num1}");
+// }
+// else
+// {
+//     Console.WriteLine($"max = {num2}");
+// }
+
+// Задача 4. Напишите программу, которая на вход принимает три числа и выдаёт максимальное
+// Console.WriteLine("Введи первое число");
+// int num1 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введи второе число");
+// int num2 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введи третье число");
+// int num3 = int.Parse(Console.ReadLine()!);
+// if (num1 > num2)
+// {
+//     if (num1 > num3) Console.WriteLine($"max = {num1} максимальное первое");
+// }
+
+// if (num2 > num1)
+// {
+//     if (num2 > num3) Console.WriteLine($"max = {num2} максимальное второе");
+// }
+// if (num3 > num1)
+// {
+//     if (num3 > num2) Console.WriteLine($"max = {num3} максимальное третье");
+// }
+
+// Задача 6. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// Console.WriteLine("Введи число");
+// int num = int.Parse(Console.ReadLine()!);
+// if (num % 2 == 0)
+// {
+//     Console.WriteLine($"{num} это четное число");
+// }
+// else
+// {
+//     Console.WriteLine($"{num} это нечетное число");
+// }
+
+// Задача 8. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N. 
+// Console.WriteLine("Введи число");
+// int n = int.Parse(Console.ReadLine()!);
+// int x = 1;
+// while (x <= n)
+// {
+//     if (x % 2 == 0) Console.Write($"{x}, ");
+//     x ++;
+// }
---
+++ Seminar1/Seminar1_zadachy.cs
@@ -0,0 +1,67 @@
+﻿// Задача про два числа: является ли первое квадратом второго
+// Console.WriteLine("Введи первое число");
+// int num1 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введи второе число");
+// int num2 = int.Parse(Console.ReadLine()!);
+// if (Math.Pow(num2, 2) == num1)
+// {
+//     Console.WriteLine($"{num1} является квадратом {num2}");
+// }
+// else
+// {
+//     Console.WriteLine("Первое число не является квадратом второго");
+// }
+
+// Задача: вывести число, умноженное само на себя
+// Console.WriteLine("Введи число");
+// int num = int.Parse(Console.ReadLine()!);
+// num = Convert.ToInt32(Math.Pow(num, 2));
+// Console.WriteLine(num);
+
+// Console.WriteLine("Введи номер дня недели от 1 до 7");
+// int num = int.Parse(Console.ReadLine()!);
+
+//     if (num == 1) {
+//         Console.WriteLine("Понедельник");
+//     }
+//     if (num == 2) {
+//         Console.WriteLine("Вторник");
+//     }
+//     if (num == 3) {
+//         Console.WriteLine("Среда");
+//     }
+    // else
+    //     Console.WriteLine("Это еще что за день?");
+    // Ошибка: Для случая "1" выводит оба варианта
+
+
+//     Console.WriteLine("Введи номер дня недели от 1 до 7");
+// int num = int.Parse(Console.ReadLine()!);
+// switch(num)
+//     {
+//         case 1: 
+//         Console.WriteLine("понедельник");
+//         break; 
+//         case 2: 
+//         Console.WriteLine("вторник");
+//         break; 
+//         default:
+//         Console.WriteLine("не знаю такого");
+//         break;
+//     }
+
+// // Задача про числа от n до -n
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine()!);
+int x = -n;
+while (x <= n)
+{
+    Console.WriteLine($"чиcло {x}");
+    x ++;
+}
+
+
+
+
+
+       
