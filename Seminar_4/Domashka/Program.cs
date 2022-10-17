e7525c8fd13d1fed43551d744674802d234b68ec
ktmka <tomatimoto@gmail.com>
2022-10-17 23:37:41 +0500

    add Seminar_4

---
+++ Seminar_4/Domashka/Program.cs
@@ -0,0 +1,92 @@
+﻿// Задача 25. Напишите цикл, который принимает на вход два числа (A и B) и 
+//возводит число A в натуральную степень B.
+// Console.WriteLine( "Введите первое число");
+// int n1 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine( "Введите второе число");
+// int n2 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine($"Число {n1} в степени {n2} = {GetPow(n1, n2)}");
+
+// int GetPow(int a, int b){       // Метод
+//     int pow = a;
+//     for (int i = 1; i < b; i++){
+//         pow = pow * a;
+//     }
+//     return pow;
+// }
+
+// ---------------------------------------------------------------------------------
+// Задача 27. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//Вариант1
+Console.WriteLine( "Введите число");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр в числе {n} = {GetSum(n)}");
+
+int GetSum(int a){       
+    string x = a.ToString();
+    int length = x.Length;
+    int sum = 0;
+    for (int i = 0; i < length; i++){
+        sum = sum + a%10;
+        a = a/10;
+    }
+    return sum;
+}
+
+
+// Вариант 2, с конвертацией string -> int32. Не работает? на строке int x = Convert.ToInt32(a[i]); 
+// Console.WriteLine( "Введите число");
+// string n = (Console.ReadLine()!);    
+// Console.WriteLine($"Сумма цифр в числе {n} = {GetSum(n)}");
+
+// int GetSum(string a){       
+//     int sum = 0;
+//     int length = a.Length;
+//     for (int i = 0; i < length; i++){
+//         int x = (Convert.ToInt32(a[i]));
+//         Console.WriteLine(x);
+//         sum = sum + x;
+//     }
+//     return sum;
+// }
+
+
+
+
+// ----------------------------------------------------------------------------
+// Задача 29. Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// Вариант 1. Массив из N элементов с рандомными значениями (взяла от 0 до 100)
+
+// Console.WriteLine( "Введите желаемое число элементов в массиве");
+// int x = int.Parse(Console.ReadLine()!);
+// int[] array = RandomArray(x);
+// Console.WriteLine($"[{String.Join(", ", array)}]");
+
+// int[] RandomArray(int size){
+//     int[] a = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         a[i] = new Random().Next(101);
+//     }
+//     return a;
+// }
+
+
+// Вариант 2. Программа спрашивает нужное кол-во элементов, затем считывает N чисел 
+//и создает из них массив 
+
+// string RandomArray(){
+//     Console.WriteLine( "Введите желаемое число элементов в массиве");
+//     int size = int.Parse(Console.ReadLine()!);
+//     int[] a = new int[size];
+//     int x = 0;
+//     for (int i = 0; i < size; i++)
+//     {
+//         Console.WriteLine( "введите число");
+//         x = int.Parse(Console.ReadLine()!);
+//         a[i] = x;
+//     }
+//     return ($"[{String.Join(", ", a)}]");
+// }
+
+// Console.WriteLine(RandomArray());
---
+++ Seminar_4/Program.cs
@@ -0,0 +1,100 @@
+﻿// Программа выводит сумму чисел от 1 до А
+// Console.WriteLine( "Введите число");
+// int A = int.Parse(Console.ReadLine()!);
+// Console.WriteLine($"Сумма чисел от 1 до {A} = {GetSum(A)}");
+
+// //Создаем метод
+// int GetSum(int limit){
+//     int sum = 0;
+//     for (int i = 1; i <= limit; i++){
+//         sum = sum + i;
+//     }
+//     return sum;
+// }
+//----------------------------------------------------------------
+// Программа выводит количество цифр в числе
+// Console.WriteLine( "Введите число");
+// int B = int.Parse(Console.ReadLine()!);
+// Console.WriteLine($"Кол-во цифр в числе  = {GetNum(B)}");
+
+// // Создаем метод
+// int GetNum(int n){
+//     int i = 0;
+//     if (n > 0){
+//         while (n > 0)
+//         {
+//             while (i < n)
+//             {
+//                 n = n/10;
+//                 i ++;
+//             }
+//         }
+//     return i;  
+//     }
+//     return 1;  
+//     }
+// Ну и капец!!!
+
+
+// Вариант со строкой - лучше
+// Console.WriteLine( "Введите число");
+// string h = (Console.ReadLine()!);
+// Console.WriteLine($"Количество цифр в {h} = {GetNum2(h)}");
+
+// //Создаем метод
+// int GetNum2(string a){
+//     int length = a.Length;
+//     return length;
+// }
+
+// ----------------------------------------------------------
+// Программа выводит произведение чисел от 1 до n
+// Console.WriteLine( "Введите число");
+// int n = int.Parse(Console.ReadLine()!);
+// Console.WriteLine($"Сумма чисел от 1 до {n} = {GetPr(n)}");
+
+// //Создаем метод
+// int GetPr(int a){
+//     int proizv = 1;
+//     for (int i = 1; i <= a; i++){
+//         proizv = proizv * i;
+//     }
+//     return proizv;
+// }
+
+// ---------------------------------------------------------------
+// Программа выводит массив из 8 элементов, заполненный 0 и 1 в случайном порядке
+// int[] a = {0,0,0,0,0,0,0,0};
+// void RandomArray(int[] array)     // Метод для вывода массива последовательно по одному значению
+// {
+//     int count = array.Length;
+//     for (int i = 0; i < count; i++)
+//     {   array[i] = new Random().Next(0, 2);
+//         Console.Write($"{array[i]} ");      // Вывод в одну строку
+//     }
+//     Console.Write(" ");                   // Пустая строка для разделения
+// }
+// RandomArray(a);
+
+//Вариант2
+// int[] a = new int[8];
+// void RandomArray(int size){
+//     for (int i = 0; i < size; i++)
+//     {
+//         a [i] = new Random().Next(0, 2);
+//     }
+// }
+// RandomArray(a.Length);
+// Console.WriteLine(String.Join(", ", a));
+
+// Вариант 3
+int[] array = RandomArray(8);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+int[] RandomArray(int size){
+    int[] a = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        a[i] = new Random().Next(2);
+    }
+    return a;
+}
