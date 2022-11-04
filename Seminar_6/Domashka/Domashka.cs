ac435b501bb6e7a7fc8b6092844e60d5a307c72e
ktmka <tomatimoto@gmail.com>
2022-11-04 14:57:12 +0500

    наконец-то добавился Seminar_6

---
+++ Seminar_6/Domashka/Domashka.cs
@@ -0,0 +1,83 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int[] array = new int[5];
+Console.WriteLine("Ожилается 5 чисел. Введите число 1");
+array[0] = int.Parse(Console.ReadLine()!);  
+Console.WriteLine("Введите число 2");
+array[1] = int.Parse(Console.ReadLine()!);  
+Console.WriteLine("Введите число 3");
+array[2] = int.Parse(Console.ReadLine()!);  
+Console.WriteLine("Введите число 4");
+array[3] = int.Parse(Console.ReadLine()!);  
+Console.WriteLine("Введите число 5");
+array[4] = int.Parse(Console.ReadLine()!);  
+
+Console.WriteLine(String.Join(", ", array));
+int res = PositivCalc(array);
+Console.WriteLine($"количество положительных чисел в массиве = {res}");     
+
+int PositivCalc(int[] array){
+    int count = 0;
+    for (int i = 0; i < array.Length; i++){
+        if (array[i] > 0)   count ++;
+    }
+    return count;
+}
+
+// -------------------------------------------------------------------
+// Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, заданных 
+//уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// Console.WriteLine("Введите k1");
+// float k1 = float.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите k2");
+// float k2 = float.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите b1");
+// float b1 = float.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите b2");
+// float b2 = float.Parse(Console.ReadLine()!);
+
+
+// Console.WriteLine(PointIntersection(k1, k2, b1, b2));
+
+// string PointIntersection(float k1, float k2, float b1, float b2){
+//     float x = (b2 - b1)/(k1 - k2);
+//     float y = k1*x + b1;
+//     float y1 = k1*x + b1;
+//     float y2 = k2*x + b2;
+//     if (y1 == y2 && k1 != k2){
+//         return ($"({x}; {y}) --> точка пересечения");
+//     }
+//     if (k1 == k2)   return "Прямые параллельны";  
+
+//     else   return "Прямые не имеют общих точек пересечения";   // на плоскости - если две прямые не имеют общих точек, то они параллельны
+// }
+
+// -----------------------------------------------------------------------------------------
+// Доп.задача
+/*Найти произведение всех элементов массива целых чисел, меньших заданного числа. 
+Размерность массива –10. Заполнение массива осуществить случайными числами от 50 до 100*/
+
+// Console.WriteLine("Введите число");
+// int x = int.Parse(Console.ReadLine()!);  // ввели число-ограничение
+
+// int[] array = GetArr(10, 50, 100);
+// Console.WriteLine(String.Join(" ", array));      // заполнили массив
+
+// int res = GetMultiplication(array, x);
+// Console.WriteLine(res);      // выполнили перемножение
+
+
+// int[] GetArr(int size, int minValue, int maxValue){
+//     int[] res = new int[size];
+//     for (int i = 0; i < size; i++){
+//         res[i] = new Random().Next(minValue, maxValue+1);
+//     }
+//     return res;
+// }
+
+// int GetMultiplication(int[] array, int limit){
+//     int result = 1;
+//     for (int i = 0; i < array.Length; i++){
+//         if (array[i] < limit)   result = result * array[i];
+//     }
+//     return result;
+// }
---
+++ Seminar_6/Seminar6.cs
@@ -0,0 +1,100 @@
+﻿
+// int[] array = GetArr(10, 0, 10);
+// Console.WriteLine(String.Join(" ", array));      // заполнили массив
+
+
+// int[] GetArr(int size, int minValue, int maxValue){
+//     int[] res = new int[size];
+//     for (int i = 0; i < size; i++){
+//         res[i] = new Random().Next(minValue, maxValue+1);
+//     }
+//     return res;
+// }
+
+// ReversArray(array);
+// Console.WriteLine(String.Join(" ", array));
+
+
+// void ReversArray(int[] a){    //способ 1, развернуть массив
+//     for (int i = 0; i < a.Length/2; i++){
+//         int k = a[i];   // временная переменная
+//         a[i] = a[a.Length - 1 - i];
+//         a[a.Length - 1 - i] = k;
+//     }
+// }
+
+// int[] res = ReversedArray(array);  //создаем массив для записи - обязательно при этом способе!
+// Console.WriteLine(String.Join(" ", res));
+
+// int[] ReversedArray(int[] b){      //  способ 2, записать данные в новый массив
+//     int[] result = new int[b.Length];
+//     for (int i = 0; i < b.Length; i++){
+//         result[i] = b[b.Length - 1 - i];
+//     }
+//     return result;
+// }
+
+
+// Преобразовать десятичное число в двоичное   - способ1
+// Console.WriteLine("Введите число");
+// int x = int.Parse(Console.ReadLine()!);
+// Console.WriteLine(Convert.ToString(x, 2));
+
+
+// Преобразовать десятичное число в двоичное   - способ2. Математически 
+// Console.WriteLine("Введите число");
+// int x = int.Parse(Console.ReadLine()!);  
+// string result = string.Empty;
+// while(x > 0){ 
+//     result = (x % 2) + result;
+//     x = x / 2;
+// }
+// Console.WriteLine(result);
+
+
+// Принять 3 числа. Существует ли треугольник с такими сторонами
+// Console.WriteLine("Введите число 1");
+// int x = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите число 2");
+// int y = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите число 3");
+// int h = int.Parse(Console.ReadLine()!);
+// Console.WriteLine(BeTriangle(x, y, h));
+// теорема неравенства: каждая из сторон треугольника меньше суммы двух других
+// bool BeTriangle(int a, int b, int c){
+//     if ((a + b) > c && (a + c) > b && (b + c) > a){
+//         return true;
+//     }
+//     else   return false;
+// }
+
+// Короткая запись
+// bool BeTriangle(int a, int b, int c){
+//     return (a + b) > c && (a + c) > b && (b + c) > a;
+// }
+// if (BeTriangle(x, y, h))   Console.WriteLine("Треугольник существует");
+// else   Console.WriteLine("Треугольник не существует");
+   
+// Копировать массив
+int[] array = GetArr(10, 0, 10);
+Console.WriteLine(String.Join(" ", array));      // заполнили массив
+
+
+int[] GetArr(int size, int minValue, int maxValue){
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++){
+        res[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return res;
+}
+
+int[] res = CopyArray(array);  //создаем массив для записи 
+Console.WriteLine(String.Join(" ", res));
+
+int[] CopyArray(int[] b){      //  записать данные в новый массив
+    int[] result = new int[b.Length];
+    for (int i = 0; i < b.Length; i++){
+        result[i] = b[i];
+    }
+    return result;
+}
