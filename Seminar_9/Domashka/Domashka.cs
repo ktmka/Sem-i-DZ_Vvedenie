9b8eda5e7d27858f4b7f79c888d4f43930645e2f
ktmka <tomatimoto@gmail.com>
2022-11-26 00:02:34 +0500

    add Seminar_9

---
+++ Seminar_9/Domashka/Domashka.cs
@@ -0,0 +1,48 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// Console.WriteLine("add number");
+// int n = int.Parse(Console.ReadLine()!);
+// Console.WriteLine(PrintNumbers(n, 1));
+
+// string PrintNumbers(int start, int end){
+//     if (start == end) return start.ToString();
+//     return (start + " " + PrintNumbers(start - 1, end));
+// }
+
+//--------------------------------------------------------------------------
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// Console.WriteLine("add number M");
+// int m = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("add number N");
+// int n = int.Parse(Console.ReadLine()!);
+// if (n < m){
+//     int temp = m;
+//     m = n;
+//     n = temp;
+// }
+// if (n == m){
+//     Console.WriteLine($"Числа равны, сумма {n} + {n} = {n * 2}");
+// }
+
+// Console.WriteLine(PrintNumbers(m, n));  // m , n входят в промежуток
+
+// int PrintNumbers(int start, int end){
+//     if (start == end) return start;
+//     return (start + PrintNumbers(start + 1, end));
+// }
+    
+//----------------------------------------------------------------------
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n
+Console.WriteLine("add number M");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("add number N");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(FunAkkerman(m, n));
+
+int FunAkkerman(int a, int b){
+    if (a == 0) return b + 1;
+    if (a != 0 && b == 0) return FunAkkerman(a - 1, 1);
+    else return FunAkkerman(a - 1, FunAkkerman(a, b - 1));
+}
+
+
+
---
+++ Seminar_9/Seminar9.cs
@@ -0,0 +1,74 @@
+﻿// int Factorial(int n){
+//     if (n == 1) return 1;
+//     return n * Factorial(n - 1);
+// }
+
+
+// Console.WriteLine("add number");
+// int n = int.Parse(Console.ReadLine()!);
+// Console.WriteLine(PrintNumbers(1, n));
+
+// string PrintNumbers(int start, int end){
+//     if (start == end) return start.ToString();
+//     return (start + " " + PrintNumbers(start + 1, end));
+// }
+
+
+
+// Console.WriteLine("add number M");
+// int m = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("add number N");
+// int n = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine(PrintNumbers(m + 1, n - 1));
+
+// string PrintNumbers(int start, int end){
+//     if (start == end) return start.ToString();
+//     return (start + " " + PrintNumbers(start + 1, end));
+// }
+
+
+// сумма цифр числа
+// Console.WriteLine("add number");
+// int m = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine(SumNumbers(m));
+
+// int SumNumbers(int limit){
+//     int sum = limit % 10;
+  
+//     if (limit < 1) return limit;
+//     else{
+//     return sum + (SumNumbers (limit / 10));
+//     }
+// }
+
+// Вариант2
+// сумма цифр числа
+// Console.WriteLine("add number");
+// int m = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine(SumNumbers(m));
+
+// int SumNumbers(int limit){
+//     if (limit < 1) return limit;
+//     else{
+//     return limit % 10 + (SumNumbers (limit / 10));
+//     }
+// }
+
+// Возвести M в степень N
+Console.WriteLine("add number M");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("add number N");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(PowNumbers(m, n));
+
+int PowNumbers(int number, int pow){
+    if (pow == 0) return 1;
+    if (pow <= 1) return number;
+    else {
+        return (number * PowNumbers(number, pow - 1));
+    }
+}
